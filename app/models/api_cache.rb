2b3e766b7678c432bf2a6dd8e0bffe5839fc5ec4
Blair Anderson <blair@repairshopr.com>
2015-04-29 10:41:35 -0700

    fix typo

--- app/models/api_cache.rb
+++ app/models/api_cache.rb
@@ -1,20 +1,20 @@
 class ApiCache < ActiveRecord::Base
 
-  IN_2_HOURS = 2.hours
+  EXPIRES_IN = 2.hours
 
   belongs_to :user
   validates_formatting_of :endpoint, using: :url
 
 
   def self.get(endpoint)
-    Rails.cache.fetch(endpoint, expires_in: IN_2_HOURS) do
+    Rails.cache.fetch(endpoint, expires_in: EXPIRES_IN) do
       response = Faraday.get(endpoint)
       response.body
     end
   end
 
   def get
-    Rails.cache.fetch("#{user_id}/#{endpoint}", expires_in: settings[:expires_in] || ::IN_2_HOURS) do
+    Rails.cache.fetch("#{user_id}/#{endpoint}", expires_in: settings[:expires_in] || ::EXPIRES_IN) do
       response = Faraday.get(endpoint)
       response.body
     end
